33126a78f9519ee4ad1739e0b09e9c44b293390c
Chad Davis <dynamiccookies@users.noreply.github.com>
2020-03-05 13:04:28 -0600

    Fix indentation

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-    //<Name of Application>
+	//<Name of Application>
 
 	//********************************************************************
 	//*********************Enter Your Variables Here**********************
 	//********************************************************************
-	
+
 	//Internal IP Address to the Server
-    $internalIP     = '';
-    
+	$internalIP     = '';
+
 	//Internal Port to the Application
 	$internalPort   = '';
-    
+
 	//External IP Address to the Server (ISP)
 	$externalIP     = '';
-    
+
 	//External Port to the Application (default = $internalPort)
 	$externalPort   = $internalPort;
-    
+
 	//Does this application use https?
 	$https          = false;
 
@@ -25,18 +25,18 @@
 	//*******************DO NOT CHANGE BELOW THIS LINE********************
 	//********************************************************************
 
-    //System Variables
-    $currentIP      = $_SERVER['REMOTE_ADDR'];
+	//System Variables
+	$currentIP      = $_SERVER['REMOTE_ADDR'];
 	$query          = $_SERVER['QUERY_STRING'];
 
 	$https ? $protocol = 'https' : $protocol = 'http';
 
 	if ($currentIP == $externalIP) {
-        $ip     = $internalIP;
-        $port   = $internalPort;
+		$ip     = $internalIP;
+		$port   = $internalPort;
 	} else {
-	    $ip     = $externalIP;
-	    $port   = $externalPort;
+		$ip     = $externalIP;
+		$port   = $externalPort;
 	}
 
 	header('Location: ' . $protocol . '://' . $ip . ':' . $port . $query);
